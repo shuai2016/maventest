21fba12721b2e73600da35dee6bfc5177d3a60ec
shuai <1627392348@qq.com>
2018-11-27 13:51:31 +0800

    继承，聚合

--- MakeFriends/src/main/java/com/shuai/maven/MakeFriends.java
+++ MakeFriends/src/main/java/com/shuai/maven/MakeFriends.java
@@ -1,7 +1,5 @@
 package com.shuai.maven;
 
-import javax.lang.model.element.VariableElement;
-
 /**
  * MakeFriends
  *
@@ -9,10 +7,10 @@ import javax.lang.model.element.VariableElement;
  * @date 2018/11/26
  */
 public class MakeFriends {
-	public String makeFriends(String name){
+	public String makeFriends(String name) {
 		HelloFriend friend = new HelloFriend();
 		friend.sayHelloToFrind("litingwei");
-		String str = "Hey,"+friend.getMyName()+" make a friend please.";
+		String str = "Hey," + friend.getMyName() + " make a friend please.";
 		System.out.println(str);
 		return str;
 	}
